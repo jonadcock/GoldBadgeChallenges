fb4a7ecbeda35376e8deccb702ec6755af9bafb4
jonadcock <JonMichaelAdcock@gmail.com>
2019-01-25 12:19:42 -0500

    update noon 1/25/19

--- 02_Challenge/ClaimRepo.cs
+++ 02_Challenge/ClaimRepo.cs
@@ -8,19 +8,38 @@ namespace _02_Challenge
 {
     public class ClaimRepo
     {
-        Queue<Claim> _data = new Queue<Claim>();
+        private Queue<Claim> _claim = new Queue<Claim>();
 
         public void AddClaimToQueue(Claim newClaim)
         {
-            _data.Enqueue(newClaim);
+
+            _claim.Enqueue(newClaim);
+
         }
-        public void RemoveClaimFromQueue()
+        public Queue<Claim> GetQueue()
         {
-            _data.Dequeue();
+            return _claim;
+        }
+        public void RemoveClaimFromQueue(Claim claim)
+        {
+            _claim.Dequeue();
+        }
+
+        public bool RemoveClaimByData(int claimID, ClaimType claimType, string description, decimal claimAmount, DateTime dOIncident, DateTime dOClaim)
+        {
+      
+            foreach(Claim claim in _claim)
+            {
+                if(claim.ClaimID == claimID)
+                {
+                    RemoveClaimFromQueue(claim);
+                    break;
+                }
+            }
         }
         public Claim NextClaim()
         {
-            Claim firstClaim = _data.Peek();
+            Claim firstClaim = _claim.Peek();
             return firstClaim;
         }
         
--- 02_Challenge/ClaimUI.cs
+++ 02_Challenge/ClaimUI.cs
@@ -9,27 +9,135 @@ namespace _02_Challenge
     public class ClaimUI
     {
         ClaimRepo _claimRepo = new ClaimRepo();
+        private Queue<Claim> _claim;
+
+        public ClaimUI()
+        {
+            _claimRepo = new ClaimRepo();
+            _claim = _claimRepo.GetQueue();
+
+        }
 
         public void Run()
         {
             bool running = true;
             while (running)
             {
-                Console.WriteLine("Komodo Claims Department. Press any key to access menu");
+
+                Console.WriteLine("Komodo Claims Department.\n" +
+                    "Press any key to access menu");
                 Console.ReadKey();
 
                 Console.WriteLine("Choose from the following options\n" +
                     "1. Enter new claim\n" +
                     "2. See all claims\n" +
-                    "3. Take care of next claim");
+                    "3. Remove claim\n" +
+                    "4. Take care of next claim");
 
-                int input = ParseInput();
+                string input = Console.ReadLine();
+                int choice = int.Parse(input);
 
-                switch (input)
+                switch (choice)
                 {
-                    case 1: AddCla
+                    case 1:
+                        AddClaimToQueue();
                          break;
+                    case 2:
+                        DisplayQueue();
+                        break;
+                    case 3:
+                        RemoveClaimFromQueue();
+                        break;
+            //        case 4:
+             //           NextClaim();
+              //          break;
                 }
+
+            }
+        }
+        private void AddClaimToQueue()
+        {
+            Claim newClaim = new Claim();
+         
+
+            Console.WriteLine("Enter claim ID number.");
+            string newClaimAsString = Console.ReadLine();
+            newClaim.ClaimID = int.Parse(newClaimAsString);
+
+            Console.WriteLine("Select claim type:\n" +
+                "1. Car\n" +
+                "2. House\n" +
+                "3. Theft");
+
+            string input = Console.ReadLine();
+            int claimType = int.Parse(input);
+
+            
+            switch (claimType)
+            {
+                default:
+                case 1:
+                    newClaim.ClaimType = ClaimType.Car;
+                    break;
+                case 2:
+                    newClaim.ClaimType = ClaimType.House;
+                    break;
+                case 3:
+                    newClaim.ClaimType = ClaimType.Theft;
+                    break;
+            }
+            Console.WriteLine("Enter claim description.");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter claim amount");
+            newClaim.ClaimAmount = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter date of incident");
+            newClaim.DOIncident = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Date of claim");
+            newClaim.DOClaim = DateTime.Parse(Console.ReadLine());
+
+            _claimRepo.AddClaimToQueue(newClaim);
+
+        }
+        private void DisplayQueue()
+        {
+            Queue<Claim> _claim = _claimRepo.GetQueue();
+
+            Console.WriteLine($"ClaimID   Type   Description     Amount   DateOfAccident   DateOfClaim    IsValid");
+            
+                foreach (Claim newClaim in _claim)
+            {
+                Console.WriteLine($"{newClaim.ClaimID}/t        {newClaim.ClaimType}/t     {newClaim.Description}/t    {newClaim.ClaimAmount}/t   {newClaim.DOIncident.ToShortDateString()}/t     {newClaim.DOClaim.ToShortDateString()}\t     {newClaim.IsValid}");
+            }
+
+        }
+        private void RemoveClaimFromQueue()
+        {
+            Console.Clear();
+            _claimRepo.GetQueue();
+
+            Console.WriteLine("Which claim would you like to remove?  Enter claim ID number and press enter.");
+            int claimID = int.Parse(Console.ReadLine());
+
+            ClaimType claimType = ClaimType.Car;
+            string description = "any";
+            decimal claimAmount = 0m;
+            DateTime dOIncident = default(DateTime);
+            DateTime dOClaim = default(DateTime);
+
+            bool success = _claimRepo.RemoveClaimByData(claimID, claimType, description, claimAmount, dOIncident, dOClaim);
+            if (success == true)
+            {
+                Console.WriteLine($"Claim #{claimID} has been removed.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"Claim #{claimID} was unable to be removed at this time.");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
     }
